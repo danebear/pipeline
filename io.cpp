e2cc35c2682e29d84c5d9c2c31b9aaec3a66fc87
danebear <chr11031@byui.edu>
2018-10-18 01:37:35 -0400

    Established line drawing code, example

--- io.cpp
+++ io.cpp
@@ -16,6 +16,54 @@
 #define S_HEIGHT    512
 #define POINTER_2D(name)        Uint32 (*name)[S_WIDTH]
 #define SETUP_POINTER_2D(name)  (Uint32 (*)[S_WIDTH])name->pixels
+#define COLOR                   Uint32    
+#define MAX_VERTICES 6
+#define ABS(in) (in > 0 ? (in) : -(in))
+
+/******************************************************
+ * Types of primitives our pipeline will render.
+ *****************************************************/
+enum PRIMITIVES 
+{
+    TRIANGLE,
+    LINE,
+    POINT
+};
+
+/*****************************************************
+ * Types of view matrices for clipping, transforms.
+ ****************************************************/
+enum VIEW_MATRICES
+{
+    PERSPECTIVE,
+    ORTHOGRAPHIC
+};
+
+/****************************************************
+ * Describes a two-dimensional point. 
+ ****************************************************/
+struct Vertex
+{
+    int x;
+    int y;
+};
+
+/*******************************************************
+ * Describes vertex-specific values for primitives.
+ ******************************************************/
+struct Attributes
+{
+    COLOR color;
+};
+
+/******************************************************
+ * Your Description goes here.
+ *****************************************************/
+struct Transform
+{
+    // Your code goes here
+};
+
 
 /************************************************************
  * UPDATE_SCREEN:
@@ -29,12 +77,14 @@ void sendFrame(SDL_Texture* GPU_OUTPUT, SDL_Renderer * ren, SDL_Surface* frameBu
     SDL_RenderPresent(ren);
 }
 
+
+
 /*************************************************************
  * POLL_CONTROLS:
  * Updates the state of the application based on:
  * keyboard, mouse, touch screen, gamepad inputs 
  ************************************************************/
-void pollUserInputs(bool & playing)
+void PollUserInputs(bool & playing)
 {
     SDL_Event e;
     while(SDL_PollEvent(&e)) 
@@ -48,6 +98,320 @@ void pollUserInputs(bool & playing)
             playing = false;
         }
     }
+}
+
+/****************************************
+ * Renders a triangle to the screen with 
+ * the appropriate fill pattern
+ ***************************************/
+void DrawTriangle(POINTER_2D(framePtr), Vertex* triangle, Attributes* attrs, int count)
+{
+    // Your code goes here 
+}
+
+/****************************************
+ * Renders a line to the screen with the
+ * appropriate coloring
+ ***************************************/
+void DrawLine(POINTER_2D(framePtr), Vertex* line, Attributes* attrs, int count)
+{
+    // Error check
+    if(count != 2) return;
+
+    // Swap so it is left-to right
+    if(line[0].x > line[1].x)
+    {
+        Vertex tmp = line[0];
+        line[0] = line[1];
+        line[1] = tmp;
+    }
+
+    // Special case horizontal lines
+    int xCurs = line[0].x;
+    int yCurs = line[0].y;
+    if(line[0].y == line[1].y)
+    {
+        while(xCurs != line[1].x)
+        {
+            framePtr[yCurs][xCurs++] = attrs[0].color;
+        }
+        return;
+    }
+
+    // Special case vertical lines
+    if(line[0].x == line[1].x)
+    {
+        int yInc = (line[0].y < line[1].y) ? 1 : -1;
+        while(xCurs != line[1].x)
+        {
+            framePtr[yCurs][xCurs] = attrs[0].color;
+            yCurs += yInc;
+        }
+        return;
+    }
+
+    // Bresenham members
+    int yDelta = ABS(line[1].y - line[0].y);
+    int xDelta = ABS(line[1].x - line[0].x);
+
+    // X vs Y Dominant members 
+    int* majorPtr, *minorPtr;
+    int majorEnd, incMinor, incMajor, DeltaMinorX2, DeltaMinorX2MinusMajorX2, errTerm;    
+    if(xDelta > yDelta)
+    {
+        majorPtr = &xCurs;
+        minorPtr = &yCurs;
+        majorEnd = line[1].x;
+        incMinor = (line[1].y > line[0].y) ? 1 : -1;
+        incMajor = 1;
+        DeltaMinorX2 = (yDelta * 2);
+        DeltaMinorX2MinusMajorX2 = DeltaMinorX2 - (xDelta * 2);
+        errTerm = DeltaMinorX2 - xDelta;
+    }
+    // Y-Dominant run
+    else
+    {
+        majorPtr = &yCurs;
+        minorPtr = &xCurs;
+        majorEnd = line[1].y;
+        incMinor = 1;
+        incMajor = (line[1].y > line[0].y) ? 1 : -1;
+        DeltaMinorX2 = (xDelta * 2);
+        DeltaMinorX2MinusMajorX2 = DeltaMinorX2 - (yDelta * 2);
+        errTerm = DeltaMinorX2 - yDelta;
+    }
+
+    framePtr[yCurs][xCurs] = attrs[0].color;
+    while(*majorPtr != majorEnd)
+    {
+        if(errTerm >= 0)
+        {            
+            *minorPtr += incMinor;
+            errTerm += DeltaMinorX2MinusMajorX2;
+        }
+        else
+        {
+            errTerm += DeltaMinorX2;
+        }
+        *majorPtr += incMajor;        
+        framePtr[yCurs][xCurs] = attrs[0].color;
+    }
+}
+
+/****************************************
+ * Renders a point to the screen with the
+ * appropriate coloring
+ ***************************************/
+void DrawPoint(POINTER_2D(framePtr), Vertex* v, Attributes* attrs, int count)
+{
+    if(count == 0) return;
+
+    // Set our pixel according to the attribute value!
+    framePtr[v->y][v->x] = attrs->color;
+}
+
+
+/*************************************************************
+ * TRANSFORM_VERTICES:
+ * Where rotations, translations, scaling operations
+ * transform the input vertices. NOTE: This does not 
+ * include the camera view transform.
+ ************************************************************/
+void TransformVertices( Vertex inputVerts[], Attributes inputAttrs[], int numInput, 
+                        Vertex transVerts[], Attributes transAttrs[], int & numTrans, 
+                        Transform* trans)
+{
+    // Dummy code - your code will replace this
+    for(numTrans = 0; numTrans < numInput; numTrans++)
+    {
+        transVerts[numTrans] = inputVerts[numTrans];
+        transAttrs[numTrans] = inputAttrs[numTrans];
+    }
+}
+
+/*************************************************************
+ * CLIP_VERTICES:
+ * Depending on our view type - clip to the frustrum that 
+ * maps to our screen's aspect ratio.  
+ ************************************************************/
+void ClipVertices(Vertex transVerts[], Attributes transAttrs[], int numTrans, 
+                  Vertex clippedVerts[], Attributes clippedAttrs[], int & numClipped, 
+                  VIEW_MATRICES view)
+{
+    // Dummy code - your code will replace this
+    for(numClipped = 0; numClipped < numTrans; numClipped++)
+    {
+        clippedVerts[numClipped] = transVerts[numClipped];
+        clippedAttrs[numClipped] = transAttrs[numClipped];
+    }
+}
+
+/*************************************************************
+ * VIEW_TRANSFORM_VERTICES:
+ * Converts our clipped geometry to screen space. 
+ * This usually means using PERSPECTIVE or ORTHOGRAPHIC 
+ * views.
+ ************************************************************/
+void ViewTransformVertices( Vertex clippedVerts[], Attributes clippedAttrs[], int numClipped, 
+                            Vertex viewVerts[], Attributes viewAttrs[], int & numView, 
+                            VIEW_MATRICES view)
+{
+    // Dummy code - your code will replace this
+    for(numView = 0; numView < numClipped; numView++)
+    {
+        viewVerts[numView] = clippedVerts[numView];
+        viewAttrs[numView] = clippedAttrs[numView];
+    }
+}
+
+/***************************************************************************
+ * Processes the indiecated PRIMITIVES type through stages of:
+ *  1) Transformation
+ *  2) Clipping
+ *  3) Perspective or Orthographic projection
+ *  4) Vertex Interpolation
+ *  5) Fragment Shading
+ **************************************************************************/
+void DrawPrimitive(PRIMITIVES type, 
+                    Vertex inputVerts[], 
+                    Attributes inputAttrs[],
+                    int numIn, 
+                    POINTER_2D(framePtr), 
+                    Transform * transFormMatrix = NULL, 
+                    VIEW_MATRICES view_m = PERSPECTIVE)
+{
+    // Matrix Transformations
+    Vertex transVerts[MAX_VERTICES];
+    Attributes transAttrs[MAX_VERTICES];
+    int numTrans;    
+    TransformVertices(inputVerts, inputAttrs, numIn, transVerts, transAttrs, numTrans, transFormMatrix); 
+
+    // Clip to our frustrum
+    Vertex clippedVerts[MAX_VERTICES];
+    Attributes clippedAttrs[MAX_VERTICES];
+    int numClipped; 
+    ClipVertices(transVerts, transAttrs, numTrans, clippedVerts, clippedAttrs, numClipped, view_m);
+
+    // View space transform
+    Vertex viewVerts[MAX_VERTICES];
+    Attributes viewAttrs[MAX_VERTICES];
+    int numView;
+    ViewTransformVertices(clippedVerts, clippedAttrs, numClipped, viewVerts, viewAttrs, numView, view_m);
+
+    // Vertex Interpolation & Fragment Drawing
+    switch(type)
+    {
+        case POINT:
+            DrawPoint(framePtr, viewVerts, viewAttrs, numView);
+            break;
+        case LINE:
+            DrawLine(framePtr, viewVerts, viewAttrs, numView);
+            break;
+        case TRIANGLE:
+            DrawTriangle(framePtr, viewVerts, viewAttrs, numView);
+            break;
+    }
+}
+
+/***********************************************
+ * Sets the screen to the indicated color value.
+ **********************************************/
+void ClearScreen(POINTER_2D(framePtr), COLOR color = 0xff000000)
+{
+    for(int y = 0; y < S_HEIGHT; y++)
+    {
+        for(int x = 0; x < S_WIDTH; x++)
+        {
+            framePtr[y][x] = color;
+        }
+    }
+}
+
+/********************************************
+ * Demonstrate Line Drawing for Project 02. 
+ *******************************************/
+void DrawLineTest(POINTER_2D(framePtr))
+{
+        Vertex vv[2];
+        Attributes aa[2];
+        aa[0].color = 0xffff0000;
+        aa[0].color = 0xffff0000;
+        vv[0].x = 300;
+        vv[0].y = 300;
+        vv[1].x = 500;
+        vv[1].y = 350;
+        DrawPrimitive(LINE, vv, aa, 2, framePtr);
+
+        vv[1].x = 350;
+        vv[1].y = 500;
+        DrawPrimitive(LINE, vv, aa, 2, framePtr);
+
+        vv[1].x = 250;
+        vv[1].y = 100;
+        DrawPrimitive(LINE, vv, aa, 2, framePtr);
+
+        vv[1].x = 100;
+        vv[1].y = 250;
+        DrawPrimitive(LINE, vv, aa, 2, framePtr);
+
+        vv[1].x = 500;
+        vv[1].y = 250;
+        DrawPrimitive(LINE, vv, aa, 2, framePtr);
+
+        vv[1].x = 100;
+        vv[1].y = 250;
+        DrawPrimitive(LINE, vv, aa, 2, framePtr);
+
+        vv[1].x = 250;
+        vv[1].y = 500;
+        DrawPrimitive(LINE, vv, aa, 2, framePtr);
+
+        vv[1].x = 350;
+        vv[1].y = 100;
+        DrawPrimitive(LINE, vv, aa, 2, framePtr);
+
+        vv[1].x = 100;
+        vv[1].y = 350;
+        DrawPrimitive(LINE, vv, aa, 2, framePtr);
+
+        // Diagonals
+        vv[0].x = 500;
+        vv[0].y = 100;
+        vv[1].x = 100;
+        vv[1].y = 500;
+        DrawPrimitive(LINE, vv, aa, 2, framePtr);
+
+        vv[0].x = 100;
+        vv[0].y = 100;
+        vv[1].x = 500;
+        vv[1].y = 500;
+        DrawPrimitive(LINE, vv, aa, 2, framePtr);
+
+        // Straights
+        vv[0].x = 100;
+        vv[0].y = 300;
+        vv[1].x = 500;
+        vv[1].y = 300;
+        DrawPrimitive(LINE, vv, aa, 2, framePtr);
+
+        vv[0].x = 300;
+        vv[0].y = 100;
+        vv[1].x = 300;
+        vv[1].y = 500;
+        DrawPrimitive(LINE, vv, aa, 2, framePtr);
+
+}
+
+/************************************************************
+ * UPDATE_SCREEN:
+ * Blits pixels from RAM to VRAM for rendering
+ ***********************************************************/
+void SendFrame(SDL_Texture* GPU_OUTPUT, SDL_Renderer * ren, SDL_Surface* frameBuf) 
+{
+    SDL_UpdateTexture(GPU_OUTPUT, NULL, frameBuf->pixels, frameBuf->pitch);
+    SDL_RenderClear(ren);
+    SDL_RenderCopy(ren, GPU_OUTPUT, NULL, NULL);
+    SDL_RenderPresent(ren);
 }
 
 /*************************************************************
@@ -77,31 +441,25 @@ int main()
     while(running) 
     {           
         // Poll for user inputs
-        pollUserInputs(running);
+        PollUserInputs(running);
 
-        // Draw Screen
-        for(int y = 0; y < S_HEIGHT; y++)
-        {
-            for(int x = 0; x < S_WIDTH; x++)
-            {
-                // Black color
-                framePtr[y][x] = 0xff000000;
-            }
-        }
+        // Refresh Screen
+        ClearScreen(framePtr);
 
-	// Draw a small box
-	for(int y = 50; y < 100; y++)
-	  {
-	    for(int x = 50; x < 100; x++)
-	      {
-		framePtr[y][x] = 0x0ffffffff;
-	      }
-	  }
+        // Draw a point on the screen 
+        Vertex v[1];
+        Attributes a[1];
+        v[0].x = 10;
+        v[0].y = 10;
+        a[0].color = 0xffff0000;
+        DrawPrimitive(POINT, v, a, 1, framePtr);
 
+        // Sample lines 
+        DrawLineTest(framePtr);
 
         // Ensure framerate at 60fps, push to screen
         SDL_Delay(17);	  
-        sendFrame(GPU_OUTPUT, REN, FRAME_BUF);
+        SendFrame(GPU_OUTPUT, REN, FRAME_BUF);
     }
 
     // Cleanup
